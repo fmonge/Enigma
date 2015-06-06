aaff0c0d61b63f6dabf2599b0f8eaf73319c2c26
gabriel_13 <gabriel14_cr@hotmail.com>
2015-06-05 22:53:33 -0600

    plugboard con comas

--- leer-cambiar-rotar.asm
+++ leer-cambiar-rotar.asm
@@ -59,251 +59,252 @@ global _start
 _start:
 
 	; Toma el argumento de la línea de comando de la pila y lo valida
-
+CargarArchivoABuffer:
 	pop rcx 	                		; Contiene el contador de argumentos
 	mov qword [ArgCount], rcx 			; Guarda la cantidad de argumentos en memoria
 
 	xor rdx,rdx 						; Contador para sacar los argumentos 
 
-SaveArgs:
-	pop qword [ArgPtrs + rdx * 8] 		; Pop an arg addr into the memory table
-	inc rdx 	                        ; Bump the counter to the next arg addr
-	cmp rdx, rcx 						; Is the counter = the argument count?
-	jb SaveArgs 						; If not, loop back and do another
+	SaveArgs:
+		pop qword [ArgPtrs + rdx * 8] 		; Pop an arg addr into the memory table
+		inc rdx 	                        ; Bump the counter to the next arg addr
+		cmp rdx, rcx 						; Is the counter = the argument count?
+		jb SaveArgs 						; If not, loop back and do another
 
-	; With the argument pointers stored in ArgPtrs, we calculate their lengths:
-	xor eax,eax 			; Searching for 0, so clear AL to 0
-	xor ebx,ebx 			; Pointer table offset starts at 0
+		; With the argument pointers stored in ArgPtrs, we calculate their lengths:
+		xor eax,eax 			; Searching for 0, so clear AL to 0
+		xor ebx,ebx 			; Pointer table offset starts at 0
 
-ScanOne:
-	mov rcx,000000000000ffffh 	; Limit search to 65535 bytes max
-	mov rdi,qword [ArgPtrs + rbx * 8]	; Put address of string to search in EDI
-	mov rdx, rdi 			; Copy starting address into EDX
+	ScanOne:
+		mov rcx,000000000000ffffh 	; Limit search to 65535 bytes max
+		mov rdi,qword [ArgPtrs + rbx * 8]	; Put address of string to search in EDI
+		mov rdx, rdi 			; Copy starting address into EDX
 
-	cld 				; Set search direction to up-memory
-	repne scasb 			; Search for null (0 char) in string at edi
-	jnz Error 			; REPNE SCASB ended without finding AL
+		cld 				; Set search direction to up-memory
+		repne scasb 			; Search for null (0 char) in string at edi
+		jnz Error 			; REPNE SCASB ended without finding AL
 
-	mov byte [rdi-1],10 		; Store an EOL where the null used to be
-	sub rdi, rdx 			; Subtract position of 0 from start address
-	mov qword [ArgLens + ebx * 8],rdi; Put length of arg into table
-	inc rbx 			        ; Add 1 to argument counter
-	cmp rbx, [ArgCount] 		; See if arg counter exceeds argument count
-	jb ScanOne 			; If not, loop back and do another one
+		mov byte [rdi-1],10 		; Store an EOL where the null used to be
+		sub rdi, rdx 			; Subtract position of 0 from start address
+		mov qword [ArgLens + ebx * 8],rdi; Put length of arg into table
+		inc rbx 			        ; Add 1 to argument counter
+		cmp rbx, [ArgCount] 		; See if arg counter exceeds argument count
+		jb ScanOne 			; If not, loop back and do another one
 
-	; Display all arguments to stdout:
-	xor rbx,rbx 			; Start (for table addressing reasons) at 0
+		; Display all arguments to stdout:
+		xor rbx,rbx 			; Start (for table addressing reasons) at 0
 
-Showem:
-	mov rsi,[ArgPtrs + 1 * 8] 			; Pasa la dirección a la que está apuntando el primer argumento al rsi
+	Showem:
+		mov rsi,[ArgPtrs + 1 * 8] 			; Pasa la dirección a la que está apuntando el primer argumento al rsi
 
-	xor r9, r9							; Contador para sacar el nombre de la pila y meterlo en un buffer
-	mov r9, 0
-
-	.ciclo1:
-		cmp byte[rsi + r9], 10			; Se compara el primer caracter de la pila con un salto de línea
-		je .siguiente					; Si es igual, pasa a revisar el otro archivo de la pila
-		mov al, byte[rsi + r9]			; Si no es igual. mueve el byte al AL
-		mov byte[NombreArch1 + r9], al	; Luego mueve lo que hay en AL, al primer byte del buffer NombreArch1
-		inc r9					
-		jmp .ciclo1						; Vuelve de nuevo al ciclo para seguir revisando la pila
-
-	.siguiente:
-
-		mov rsi,[ArgPtrs + 2 * 8] 	; Pasa la dirección a la que está apuntando el segundo argumento al rsi
-
-		xor r9, r9 ; Contador para sacar el nombre de la pila y meterlo en el buffer
+		xor r9, r9							; Contador para sacar el nombre de la pila y meterlo en un buffer
 		mov r9, 0
 
-	.ciclo2:
-		cmp byte[rsi + r9], 10          ;Se compara el primer caracter de la pila con un salto de linea
-		je .abrir_leer                  ; Si es igual, pasa a revisar el otro archivo de la pila
-		mov al, byte[rsi + r9]          ; Si no es igual. mueve el byte al AL
-		mov byte[NombreArch2 + r9], al  ; Luego mueve lo que hay en AL, al primer byte del buffer NombreArch1
-		inc r9
-		jmp .ciclo2                     ; Vuelve de nuevo al ciclo para seguir revisando la pila
+		.ciclo1:
+			cmp byte[rsi + r9], 10			; Se compara el primer caracter de la pila con un salto de línea
+			je .siguiente					; Si es igual, pasa a revisar el otro archivo de la pila
+			mov al, byte[rsi + r9]			; Si no es igual. mueve el byte al AL
+			mov byte[NombreArch1 + r9], al	; Luego mueve lo que hay en AL, al primer byte del buffer NombreArch1
+			inc r9					
+			jmp .ciclo1						; Vuelve de nuevo al ciclo para seguir revisando la pila
 
-	.abrir_leer:
+		.siguiente:
 
-	; Abrir y leer el archivo 1
-		mov rax, 2
-		mov rdi, NombreArch1
-		mov rsi, 0
-		mov rdx, 0
-		syscall
+			mov rsi,[ArgPtrs + 2 * 8] 	; Pasa la dirección a la que está apuntando el segundo argumento al rsi
 
-		mov rbx, rax
+			xor r9, r9 ; Contador para sacar el nombre de la pila y meterlo en el buffer
+			mov r9, 0
 
-		mov rax, 0
-		mov rdi, rbx
-		mov rsi, textoFinal
-		mov rdx, lenTextoFinal
-		syscall
+		.ciclo2:
+			cmp byte[rsi + r9], 10          ;Se compara el primer caracter de la pila con un salto de linea
+			je .abrir_leer                  ; Si es igual, pasa a revisar el otro archivo de la pila
+			mov al, byte[rsi + r9]          ; Si no es igual. mueve el byte al AL
+			mov byte[NombreArch2 + r9], al  ; Luego mueve lo que hay en AL, al primer byte del buffer NombreArch1
+			inc r9
+			jmp .ciclo2                     ; Vuelve de nuevo al ciclo para seguir revisando la pila
 
-	; Abrir y leer el archivo 2 
-		mov rax, 2
-		mov rdi, NombreArch2
-		mov rsi, 0
-		mov rdx, 0
-		syscall
+		.abrir_leer:
 
-		mov rbx, rax
-
-		mov rax, 0
-		mov rdi, rbx
-		mov rsi, textoFinal2
-		mov rdx, lenTextoFinal2
-		syscall
-
-		mov rax, 3
-		syscall
-
-		xor r10, r10
-		mov r10, -1
-
-		xor r9, r9
-		mov r9, 0
-
-	.limpiar_orden_rotores:
-		inc r10
-		cmp byte[textoFinal + r10], 10
-		je .limpiar_comienzo_rotores_aux
-		cmp byte[textoFinal + r10], ' '
-		je .limpiar_orden_rotores
-		mov al, byte[textoFinal + r10]   ; Si no es igual. mueve el byte al AL
-		mov byte[buffRotoresSeleccionados + r9], al  ; Luego mueve lo que hay en AL, al primer byte del buffer
-		inc r9
-		jmp .limpiar_orden_rotores      ; Vuelve de nuevo al ciclo para seguir revisando la pila
-
-	.limpiar_comienzo_rotores_aux:
-
-		xor r9, r9
-		mov r9, 0
-
-	.limpiar_comienzo_rotores:
-		inc r10
-		cmp byte[textoFinal + r10], 10
-		je .limpiar_plugboard_aux
-		cmp byte[textoFinal + r10], ' '
-		je .limpiar_comienzo_rotores
-		mov al, byte[textoFinal + r10]   	; Si no es igual. mueve el byte al AL
-		mov byte[buffDesplazamientoRotores + r9], al  ; Luego mueve lo que hay en AL, al primer byte
-		inc r9
-		jmp .limpiar_comienzo_rotores      ; Vuelve de nuevo al ciclo para seguir revisando la pila
-
-	.limpiar_plugboard_aux:
-
-		xor r9, r9
-		mov r9, 0
-
-	.limpiar_plugboard:
-		inc r10
-		cmp byte[textoFinal + r10], 0h
-		je .exit
-		;je .imprimir
-		cmp byte[textoFinal + r10], ' '
-		je .limpiar_plugboard
-		cmp byte[textoFinal + r10], ','
-		je .limpiar_plugboard
-		mov al, byte[textoFinal + r10]   ; Si no es igual. mueve el byte al AL
-		mov byte[plugboard + r9], al  ; Luego mueve lo que hay en AL, al primer byte 
-		inc r9
-		jmp .limpiar_plugboard      ; Vuelve de nuevo al ciclo para seguir revisando la pila
-
-	
-	.imprimir:
-		;Se imprimen los buffer para ver si están bien
-
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, buffRotoresSeleccionados
-			mov rdx, lenBuffRotores
+		; Abrir y leer el archivo 1
+			mov rax, 2
+			mov rdi, NombreArch1
+			mov rsi, 0
+			mov rdx, 0
 			syscall
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
+			mov rbx, rax
+
+			mov rax, 0
+			mov rdi, rbx
+			mov rsi, textoFinal
+			mov rdx, lenTextoFinal
 			syscall
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, buffDesplazamientoRotores
-			mov rdx, lenBuffDesplazamientoRotores
+		; Abrir y leer el archivo 2 
+			mov rax, 2
+			mov rdi, NombreArch2
+			mov rsi, 0
+			mov rdx, 0
 			syscall
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
+			mov rbx, rax
+
+			mov rax, 0
+			mov rdi, rbx
+			mov rsi, textoFinal2
+			mov rdx, lenTextoFinal2
 			syscall
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, plugboard
-			mov rdx, lenPlugboard
+			mov rax, 3
 			syscall
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
-			syscall
-			
-			
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, buffRotorUno
-			mov rdx, 27
-			syscall
+			xor r10, r10
+			mov r10, -1
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
-			syscall
-			
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, buffRotorDos
-			mov rdx, 27
-			syscall
+			xor r9, r9
+			mov r9, 0
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
-			syscall
-			
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, buffRotorTres
-			mov rdx, 27
-			syscall
+		.limpiar_orden_rotores:
+			inc r10
+			cmp byte[textoFinal + r10], 10
+			je .limpiar_comienzo_rotores_aux
+			cmp byte[textoFinal + r10], ' '
+			je .limpiar_orden_rotores
+			mov al, byte[textoFinal + r10]   ; Si no es igual. mueve el byte al AL
+			mov byte[buffRotoresSeleccionados + r9], al  ; Luego mueve lo que hay en AL, al primer byte del buffer
+			inc r9
+			jmp .limpiar_orden_rotores      ; Vuelve de nuevo al ciclo para seguir revisando la pila
 
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
-			syscall
-			
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, saltoLinea
-			mov rdx, saltoLineaLargo
-			syscall
+		.limpiar_comienzo_rotores_aux:
 
-		.exit:
-			;jmp Exit
+			xor r9, r9
+			mov r9, 0
+
+		.limpiar_comienzo_rotores:
+			inc r10
+			cmp byte[textoFinal + r10], 10
+			je .limpiar_plugboard_aux
+			cmp byte[textoFinal + r10], ' '
+			je .limpiar_comienzo_rotores
+			mov al, byte[textoFinal + r10]   	; Si no es igual. mueve el byte al AL
+			mov byte[buffDesplazamientoRotores + r9], al  ; Luego mueve lo que hay en AL, al primer byte
+			inc r9
+			jmp .limpiar_comienzo_rotores      ; Vuelve de nuevo al ciclo para seguir revisando la pila
+
+		.limpiar_plugboard_aux:
+
+			xor r9, r9
+			mov r9, 0
+
+		.limpiar_plugboard:
+			inc r10
+			cmp byte[textoFinal + r10], 0h
+			;je .exit
+			je .imprimir
+			cmp byte[textoFinal + r10], ' '
+			je .limpiar_plugboard
+		;	cmp byte[textoFinal + r10], ','
+		;	je .limpiar_plugboard
+			mov al, byte[textoFinal + r10]   ; Si no es igual. mueve el byte al AL
+			mov byte[plugboard + r9], al  ; Luego mueve lo que hay en AL, al primer byte 
+			inc r9
+			jmp .limpiar_plugboard      ; Vuelve de nuevo al ciclo para seguir revisando la pila
+
+		
+		.imprimir:
+			;Se imprimen los buffer para ver si están bien
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, buffRotoresSeleccionados
+				mov rdx, lenBuffRotores
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, buffDesplazamientoRotores
+				mov rdx, lenBuffDesplazamientoRotores
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, plugboard
+				mov rdx, lenPlugboard
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+				
+				
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, buffRotorUno
+				mov rdx, 27
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+				
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, buffRotorDos
+				mov rdx, 27
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+				
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, buffRotorTres
+				mov rdx, 27
+				syscall
+
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+				
+				mov rax, 1
+				mov rdi, 1
+				mov rsi, saltoLinea
+				mov rdx, saltoLineaLargo
+				syscall
+
+			.exit:
+				;jmp Exit
 
 call CargarRotoresSeleccionados
-call rotarRotores
+call RotarRotores
 
 Exit: 
 	mov rax, 60 			; Code for Exit Syscall
 	mov rdi, 0 			; Return a code of zero
 	syscall
 	
+
 
 Error: 
 	push rax
